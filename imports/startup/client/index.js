ba409b578e45e80e5189066e26d14355962f1350
Hugh Willson <hugh@octonary.com>
2016-10-29 11:13:23 -0400

    Removed redirect to checkout URL if missing payment ID, instead redirecting to store homepage.

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -7,7 +7,7 @@ import { Meteor } from 'meteor/meteor';
 import PaymentContainer from '../../ui/containers/PaymentContainer';
 
 // If a payment ID is passed in, render the payment page, otherwise
-// redirect to the stores checkout page.
+// redirect to the stores homepage.
 const paymentId = window.location.search.replace('?id=', '');
 if (paymentId) {
   render(
@@ -15,5 +15,5 @@ if (paymentId) {
     document.getElementById('app')
   );
 } else {
-  window.location.replace(Meteor.settings.public.paymentPage.checkoutUrl);
+  window.location.replace(Meteor.settings.public.paymentPage.siteUrl);
 }
