52feb26fd3b65a9ae20daaf3805a03d2cd10cbcb
Hugh Willson <hugh@octonary.com>
2016-12-13 12:14:28 -0500

    Moved server reference for stripe helper down a bit to get rid of client errors.

--- imports/api/payments/collection.js
+++ imports/api/payments/collection.js
@@ -1,13 +1,12 @@
 import { Mongo } from 'meteor/mongo';
 
-import StripeHelper from '../cards/server/stripe_helper';
-
 const Payments = new Mongo.Collection('payments');
 
 Payments.refund = (invoiceId) => {
   if (invoiceId) {
     const payment = Payments.findOne({ x_invoice: `#${invoiceId}` });
     if (payment && payment.charge.id) {
+      import StripeHelper from '../cards/server/stripe_helper';
       StripeHelper.refundCharge(payment.charge.id);
     }
   }
