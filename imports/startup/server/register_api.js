c9eff45d0d8b46efdfe6dab8ff6419f44e2b814d
Hugh Willson <hugh@octonary.com>
2017-03-06 15:14:07 -0500

    Removed un-needed cron reference.

--- imports/startup/server/register_api.js
+++ imports/startup/server/register_api.js
@@ -2,7 +2,3 @@ import '../../api/environment/methods';
 import '../../api/payments/methods';
 import '../../api/payments/server/publications';
 import '../../api/cards/methods';
-
-
-
-import '../../api/shopify/server/retry_pending_order_billing';
