32ee65a8bbaaef82b3182aac878cf6e91828a6e3
Hugh Willson <hugh@octonary.com>
2017-05-08 21:16:25 -0400

    Updated to pull free trial details from note_attributes.

--- imports/api/subscriptions/server/subscription.js
+++ imports/api/subscriptions/server/subscription.js
@@ -48,20 +48,24 @@ const Subscription = {
           this._subscriptionFrequencyId =
             lineItem.sku.replace('TF_SUB_', '').toLowerCase();
         } else if (lineItem.sku.indexOf('TF_TRIAL_') > -1) {
-          // Note format:
-          // TF_ONGOING_TRIAL: TF_SPORT_SIZE (PRODUCT_ID-VARIATION_ID)
-          const note = orderData.note;
-          const matches = /^TF_ONGOING_TRIAL.*?\((.*?)-(.*?)\)/.exec(note);
-          if (matches && (matches.length === 3)) {
-            const productId = matches[1];
-            const variationId = matches[2];
-            productData.products.push({
-              productId,
-              variationId,
-              quantity: 1,
-            });
-            productData.includesFreeTrial = true;
-          }
+          // note_attributes format:
+          // name = TF_ONGOING_TRIAL
+          // value = TF_SPORT_SIZE (PRODUCT_ID-VARIATION_ID)
+          orderData.note_attributes.forEach((note) => {
+            if (note.name === 'TF_ONGOING_TRIAL') {
+              const matches = /^TF_.*?\((.*?)-(.*?)\)/.exec(note.value);
+              if (matches && (matches.length === 3)) {
+                const productId = matches[1];
+                const variationId = matches[2];
+                productData.products.push({
+                  productId,
+                  variationId,
+                  quantity: 1,
+                });
+                productData.includesFreeTrial = true;
+              }
+            }
+          });
         } else {
           productData.products.push({
             productId: lineItem.product_id,
